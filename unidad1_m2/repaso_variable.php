14ed4bda6b890fc7278dc60fa6f8c35c7b7f4749
Mario Barberis <mariojbarberis@hotmail.com>
2023-03-26 13:01:18 -0300

    inicio 2

--- unidad1_m2/repaso_variable.php
+++ unidad1_m2/repaso_variable.php
@@ -1,6 +1,7 @@
 <?php
 //numerico
 $numero = 4;
+echo "<br>";
 echo "Esto es una variable numerica <br>";
 var_dump($numero);  //me da mas info de la variable, ej si es entero y que numero es
 
@@ -22,9 +23,23 @@ echo "<hr>";
 
 //arrays
 $colores = array("rojo","azul","verde");
+echo "<br>";
 echo "Esto es un array:$colores[1]<br>";
 var_dump($colores);
 
 echo "<hr>";
 
 //arrays con propiedades
+$verduras = array ("verdura1" => "lechuga", "verdura2" => "zanahoria", "verdura3" => "morron");
+echo "<br>";
+echo "Esto es un array con propiedades: $verduras[1]";
+echo "<br>";
+var_dump($verduras);
+
+echo "<hr>";
+
+//Variables objetos
+$frutas = (object) ["fruta1"=>"pera", "fruta2"=>"manzana"];
+echo "Esto es una variable objeto: $frutas->fruta1 <br>";
+var_dump($frutas);
+
