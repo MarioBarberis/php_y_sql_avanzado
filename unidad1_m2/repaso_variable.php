86cc94a6f256bfde00044f176eeeb384024c8394
Mario Barberis <mariojbarberis@hotmail.com>
2023-03-25 23:12:08 -0300

    inicio

---
+++ unidad1_m2/repaso_variable.php
@@ -0,0 +1,30 @@
+<?php
+//numerico
+$numero = 4;
+echo "Esto es una variable numerica <br>";
+var_dump($numero);  //me da mas info de la variable, ej si es entero y que numero es
+
+echo "<hr>";
+
+//string
+$palabra = "Esto es un string";
+echo "<br>".$palabra."<br>";
+var_dump($palabra); 
+
+echo "<hr>";
+
+//boleana
+$boleana = true;
+echo "<br>".$boleana."<br>";
+var_dump($boleana); 
+
+echo "<hr>";
+
+//arrays
+$colores = array("rojo","azul","verde");
+echo "Esto es un array:$colores[1]<br>";
+var_dump($colores);
+
+echo "<hr>";
+
+//arrays con propiedades
