86f114137897344a9eed06db0c4dd72c6aa08408
Mario Barberis <mariojbarberis@hotmail.com>
2023-04-13 13:12:04 -0300

    correccion

---
+++ modulo2_unidad1/vistas/paginas/finalizar.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Finalizar</title>
+</head>
+<body>
+
+<center><h1 class="tituloPagina"> FINALIZAR PEDIDOS </h1></center>
+
+<form action="bdinterno.php" method="post" class="formufinal">
+
+<input type="text" name="nombre1" placeholder="Nombre del producto procesado">
+<input type="number" name="valor1" placeholder="Valor final del Producto">
+<textarea name="comentario1" id="" cols="30" rows="10" placeholder="Comentarios"></textarea>
+<input type="submit" value="Enviar a Base de Datos Interna">
+
+</form>
+
+    
+</body>
+</html>
