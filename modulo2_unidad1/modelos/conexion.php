26567dc94c86984b151dd7d5787850b0f7d0e4a2
Mario Barberis <mariojbarberis@hotmail.com>
2023-04-21 11:53:27 -0300

    correcion

--- modulo2_unidad1/modelos/conexion.php
+++ modulo2_unidad1/modelos/conexion.php
@@ -2,7 +2,7 @@
 
 class Conexion{
     static public function conectar(){
-        $link = new PDO("mysql:host=localhost;port=80;dbname=php_avanzado", "root", "");
+        $link = new PDO("mysql:host=localhost;port=3306;dbname=php_avanzado", "root", "");
         $link->exec("set names utf8");
         return $link; 
     }
